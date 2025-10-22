138912a26f1dc166fa3434c65804e0d277b9e013
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2025-10-22 15:08:40 -0300

    logo

--- 24-crud/frontend/src/components/template/Logo.jsx
+++ 24-crud/frontend/src/components/template/Logo.jsx
@@ -1,9 +1,13 @@
 import './Logo.css'
-
+import LogoIMG from '../../assets/images/logo.png'
 import React from 'react'
 
 export default function Logo() {
   return (
-    <aside className='logo'>Logo</aside>
+    <aside className='logo'>
+      <a href="/" className="logo">
+        <img src={LogoIMG} alt="Logo" srcset="" />
+      </a>
+    </aside>
   )
 }
