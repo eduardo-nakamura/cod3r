a4e494e8dfddb7bf45f89c4ca7fa5eb1ded22d00
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2025-11-06 16:22:02 -0300

    ecma scrpit

---
+++ 27-webpack-legado/ex/index.js
@@ -0,0 +1,7 @@
+// const logger = require('./logger')
+// logger.info('1tetete sss')
+// require('./duvida')
+import Pessoa from './pessoa'
+
+const guilherme = new Pessoa("Guilherme")
+alert(guilherme.toString())
