c66eec6a069abf505f3518db3c05f8d72819651f
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2025-11-07 15:53:01 -0300

    webpack-legado

--- 27-webpack-legado/ex/index.js
+++ 27-webpack-legado/ex/index.js
@@ -1,7 +1,30 @@
 // const logger = require('./logger')
 // logger.info('1tetete sss')
 // require('./duvida')
-import Pessoa from './pessoa'
+// import Pessoa from './pessoa'
 
-const guilherme = new Pessoa("Guilherme")
-alert(guilherme.toString())
+// const guilherme = new Pessoa("Guilherme")
+// console.log(guilherme.toString())
+
+// const produto = {
+//     nome: 'Caneta',
+//     preco: 1.90,
+//     desconto: 0.05
+// }
+
+// function clone(obj){
+//     return { ...obj }
+// }
+
+// const caderno = clone(produto)
+// caderno.nome = 'Caderno'
+// console.log(produto)
+// console.log(caderno)
+import './style.css'
+import React from 'react'
+
+export default function index() {
+  return (
+    <div>Funcionour</div>
+  )
+}
