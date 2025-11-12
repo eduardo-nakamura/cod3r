78b78200752e3f8d88e47aa33913760f51d43a1a
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2025-11-12 15:46:42 -0300

    listagem capitulos

--- 28-react-legado/ex/index.jsx
+++ 28-react-legado/ex/index.jsx
@@ -5,5 +5,16 @@ import Field from '../aulas/ex8/Field'
 ReactDOM.render(
     <div>
         <Field initialValue="teste" />
+        1 - Instalação Packages
+        2 - Criação e configuração webpack
+        3 - Criação pasta public e index.html
+        4 - Criação pasta ex e componente index.jsx (Renderizar o componente de index.jsx na div com id app do index.html)
+        5 - Criação do script dev no package.json
+        6 - Ex. 2 Criação de componente.jsx.
+        7 - colocar resolve extenstions no webpack para resolver extensões automaticamente
+        8 - Ex. 3 - exportando Mais de um componente
+        9 - Ex. 4 - Criação de Componente Filho (member.jsx) e Componente pai (family.jsx)
+        10 - Ex.5 - Passando Propriedades para componente Filhos
+        11 - Ex.6 - Criação de Componente Classe (ClassComponent.jsx) com contador
     </div>
-, document.getElementById('app'))
+    , document.getElementById('app'))
